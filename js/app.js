7d9a9634c5b924c0d956c29380b56623a7886b64
YaaraOP <mannupahari157@gmail.com>
2026-06-30 11:20:49 +0530

    update

--- js/app.js
+++ js/app.js
@@ -70,6 +70,20 @@ menuBtn.innerHTML='<i class="fa-solid fa-bars"></i>';
 };
 
 });
+document.addEventListener("click",(e)=>{
+
+if(
+!nav.contains(e.target) &&
+!menuBtn.contains(e.target)
+){
+
+nav.classList.remove("active");
+
+menuBtn.innerHTML='<i class="fa-solid fa-bars"></i>';
+
+}
+
+});
 
 /* Scroll Top */
 
